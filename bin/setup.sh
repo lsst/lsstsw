b97d4f812a41d7c6c7425897b3d3a2dbfc727bea
Mario Juric <mjuric@lsst.org>
2014-08-19 02:14:47 -0500

    bin/deploy: Enable in-place upgrade of EUPS

--- bin/setup.sh
+++ bin/setup.sh
@@ -10,7 +10,12 @@ export PATH="$LSSTSW/bin:$PATH"
 
 export MANPATH="$LSSTSW/lfs/share/man:"
 
-. $LSSTSW/eups/bin/setups.sh
+if [[ ! -f $LSSTSW/eups/current/bin/setups.sh ]]; then
+    echo "error: eups not in $LSSTSW/eups/current; rerun bin/deploy."
+    exit -1
+fi
+
+. $LSSTSW/eups/current/bin/setups.sh
 
 setup -r $LSSTSW/lsst_build
 
