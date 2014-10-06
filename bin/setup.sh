55b631c2d825f79ede33cee0e17d1ff0999a7a3a
Mario Juric <mjuric@lsst.org>
2014-10-05 20:27:12 -0500

    bin/setup.sh: don't exit the shell if EUPS is not found

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,16 +4,18 @@
 #
 LSSTSW=${LSSTSW:-$HOME}
 
+if [[ ! -f $LSSTSW/eups/current/bin/setups.sh ]]; then
+    echo "error: eups not found in $LSSTSW/eups/current" 1>&2
+    echo "verify \$LSSTSW points to the directory where lsstsw has been cloned," 1>&2
+    echo "  or rerun bin/deploy to redeploy EUPS." 1>&2
+    return
+fi
+
 export PATH="$LSSTSW/anaconda/bin:$PATH"
 export PATH="$LSSTSW/lfs/bin:$PATH"
 export PATH="$LSSTSW/bin:$PATH"
 
 export MANPATH="$LSSTSW/lfs/share/man:"
-
-if [[ ! -f $LSSTSW/eups/current/bin/setups.sh ]]; then
-    echo "error: eups not in $LSSTSW/eups/current; rerun bin/deploy."
-    exit -1
-fi
 
 . $LSSTSW/eups/current/bin/setups.sh
 
