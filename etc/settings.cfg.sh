aa3ebc59561ac3a8aee2edda9f3f107b4ee09c82
Joshua Hoblitt <jhoblitt@cpan.org>
2015-04-01 14:27:40 -0700

    (DM-2466) unexport variables with no external usage
    Preparation for removing the requirement for the user to declare LSSTSW
    properly in their environment.

--- etc/settings.cfg.sh
+++ etc/settings.cfg.sh
@@ -3,12 +3,12 @@
 #
 
 # top-level products
-export PRODUCTS=${PRODUCTS:-"lsst_sims lsst_distrib qserv_distrib git anaconda webserv"}
+PRODUCTS=${PRODUCTS:-"lsst_sims lsst_distrib qserv_distrib git anaconda webserv"}
 
 # set it to nonempty to prevent versiondb from being pushed upstream
 # unless you're the automated LSST software account
 if [[ $USER != 'lsstsw' || $(hostname) != 'lsst-dev.ncsa.illinois.edu' ]]; then
-    export NOPUSH=1
+    NOPUSH=1
 fi
 
 #
@@ -16,19 +16,23 @@ fi
 #
 
 # where are we? default to $HOME if $LSSTSW hasn't been defined
-export LSSTSW=${LSSTSW:-$HOME/lsstsw}
-
-# use 'package' for public releases, use 'git' for development releases
-export EUPSPKG_SOURCE=${EUPSPKG_SOURCE:-git}
-
-# the location of the distribution server
-export EUPS_PKGROOT=$LSSTSW/distserver/production
+LSSTSW=${LSSTSW:-$HOME/lsstsw}
 
 # the location of source repositories
-export REPOSFILE=${LSSTSW}/etc/repos.yaml
+REPOSFILE=${LSSTSW}/etc/repos.yaml
 
 # location of the build directory
-export BUILD_DIR=${BUILD_DIR:-${LSSTSW}/build}
+BUILD_DIR=${BUILD_DIR:-${LSSTSW}/build}
+
+# location of the version repository (it should be a clone of git@git.lsstcorp.org/LSST/DMS/devenv/versiondb.git)
+VERSIONDB=$LSSTSW/versiondb
+
+# location of exclusions.txt file for 'lsst-build prepare' command
+EXCLUSIONS=$LSSTSW/etc/exclusions.txt
+
+#
+# exported variables
+#
 
 # repository path for 'eups distrib create'
 export EUPSPKG_REPOSITORY_PATH="$BUILD_DIR"/'$PRODUCT'
@@ -36,8 +40,8 @@ export EUPSPKG_REPOSITORY_PATH="$BUILD_DIR"/'$PRODUCT'
 # location of the EUPS stack
 export EUPS_PATH=$LSSTSW/stack
 
-# location of the version repository (it should be a clone of git@git.lsstcorp.org/LSST/DMS/devenv/versiondb.git)
-export VERSIONDB=$LSSTSW/versiondb
+# use 'package' for public releases, use 'git' for development releases
+export EUPSPKG_SOURCE=${EUPSPKG_SOURCE:-git}
 
-# location of exclusions.txt file for 'lsst-build prepare' command
-export EXCLUSIONS=$LSSTSW/etc/exclusions.txt
+# the location of the distribution server
+export EUPS_PKGROOT=$LSSTSW/distserver/production
