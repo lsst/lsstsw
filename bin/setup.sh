8cb55c8c3f1b005fcab0bc46e1f3fe88ae82c120
Tim Jenness <tjenness@lsst.org>
2016-02-26 12:41:49 -0700

    $LSSTSW may include spaces

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,7 +5,7 @@
 # relative to <lsstsw>/bin/
 LSSTSW=$(cd "$(dirname "$BASH_SOURCE")/.."; pwd)
 
-if [[ ! -f $LSSTSW/eups/current/bin/setups.sh ]]; then
+if [[ ! -f "$LSSTSW/eups/current/bin/setups.sh" ]]; then
     echo "error: eups not found in $LSSTSW/eups/current" 1>&2
     echo "  you may need to [re]run bin/deploy to [re]deploy EUPS." 1>&2
     return
@@ -17,8 +17,8 @@ export PATH="$LSSTSW/bin:$PATH"
 
 export MANPATH="$LSSTSW/lfs/share/man:"
 
-. $LSSTSW/eups/current/bin/setups.sh
+. "$LSSTSW/eups/current/bin/setups.sh"
 
-setup -r $LSSTSW/lsst_build
+setup -r "$LSSTSW/lsst_build"
 
 echo "notice: lsstsw tools have been set up."
