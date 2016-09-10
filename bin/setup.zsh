5e33ea217badd1a602bbbcdf923e1268d19d4906
Josh Meyers <jmeyers314@gmail.com>
2016-09-09 18:59:31 -0700

    Fix up setup.zsh script.
    - Use quotes.
    - Point to .zsh eups scripts.
    - Use simpler $BASH_SOURCE equivalent.

--- bin/setup.zsh
+++ bin/setup.zsh
@@ -1,11 +1,11 @@
 # setup lsstsw environment
 #
-# source this file from your ~/.bashrc
+# source this file from your ~/.zshrc
 #
 # relative to <lsstsw>/bin/
-LSSTSW=$(cd "$(dirname "${(%):-%x}")/.."; pwd)
+LSSTSW=$(cd "$(dirname "$0")/.."; pwd)
 
-if [[ ! -f $LSSTSW/eups/current/bin/setups.sh ]]; then
+if [[ ! -f "$LSSTSW/eups/current/bin/setups.zsh" ]]; then
     echo "error: eups not found in $LSSTSW/eups/current" 1>&2
     echo "  you may need to [re]run bin/deploy to [re]deploy EUPS." 1>&2
     return
@@ -17,8 +17,8 @@ export PATH="$LSSTSW/bin:$PATH"
 
 export MANPATH="$LSSTSW/lfs/share/man:"
 
-. $LSSTSW/eups/current/bin/setups.sh
+. "$LSSTSW/eups/current/bin/setups.zsh"
 
-setup -r $LSSTSW/lsst_build
+setup -r "$LSSTSW/lsst_build"
 
 echo "notice: lsstsw tools have been set up."
