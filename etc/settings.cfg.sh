ae843282eb4f91259821295cf6456d4a706556d8
Kian-Tat Lim <ktl@slac.stanford.edu>
2021-02-03 17:47:01 -0800

    Update rubin-env to 0.3.1.

--- etc/settings.cfg.sh
+++ etc/settings.cfg.sh
@@ -6,7 +6,7 @@
 #
 
 # scipipe-conda-env reference
-LSST_SPLENV_REF=${LSST_SPLENV_REF:-0.2.1}
+LSST_SPLENV_REF=${LSST_SPLENV_REF:-0.3.1}
 SPLENV_BASE_NAME="lsst-scipipe"
 LSST_SPLENV_REPO=${LSST_SPLENV_REPO:-https://github.com/lsst/scipipe_conda_env.git}
 
